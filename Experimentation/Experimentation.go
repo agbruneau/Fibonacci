9bf697b6d20eb7a5bcc6808dc5270396f5843909
André-Guy Bruneau <45982482+agbruneau@users.noreply.github.com>
2024-10-25 10:01:54 -0400

    Add Readme dans Experimentation et OLD existant

--- Experimentation/Experimentation.go
+++ Experimentation/Experimentation.go
@@ -146,7 +146,7 @@ func formatBigIntSci(n *big.Int) string {
 	return fmt.Sprintf("%se%d", formattedNum, exponent)
 }
 func main() {
-	n := 1000000 // 100 000
+	n := 100000 // 100 000
 	n = n - 1
 	numWorkers := runtime.NumCPU()
 	segmentSize := n / numWorkers
