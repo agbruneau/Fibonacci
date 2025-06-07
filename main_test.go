9ca3ae848e665b9e44d2aab74acba5464c9c8e1d
André-Guy Bruneau <45982482+agbruneau@users.noreply.github.com>
2025-06-07 05:25:26 -0400

    Update

--- main_test.go
+++ main_test.go
@@ -37,6 +37,21 @@ func TestFibBinet(t *testing.T) {
 		})
 	}
 }
+func TestFibBinetLarge(t *testing.T) {
+	pool := newIntPool()
+	n := 1000
+	resFast, err := fibFastDoubling(context.Background(), nil, n, pool)
+	if err != nil {
+		t.Fatalf("fastdoubling error: %v", err)
+	}
+	resBinet, err := fibBinet(context.Background(), nil, n, pool)
+	if err != nil {
+		t.Fatalf("binet error: %v", err)
+	}
+	if resBinet.Cmp(resFast) != 0 {
+		t.Errorf("Binet wrong for %d", n)
+	}
+}
 
 func TestFibFastDoubling(t *testing.T) {
 	pool := newIntPool()
